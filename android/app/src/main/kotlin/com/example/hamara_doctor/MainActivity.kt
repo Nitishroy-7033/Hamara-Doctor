485d430de879504b75f1522ae3acfc13173d755c
Nitish Kumar ( Flutter hero ) <86263713+Nitishroy-7033@users.noreply.github.com>
2026-01-17 01:01:54 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/hamara_doctor/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.hamara_doctor
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/core/theme/app_theme.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class AppTheme {
+  static final ThemeData lightTheme = ThemeData(
+    useMaterial3: true,
+    colorScheme: ColorScheme.fromSeed(
+      seedColor: Colors.blue,
+      primary: Colors.blue,
+      brightness: Brightness.light,
+    ),
+    textTheme: GoogleFonts.interTextTheme(),
+    appBarTheme: const AppBarTheme(
+      centerTitle: true,
+      elevation: 0,
+      backgroundColor: Colors.transparent,
+      foregroundColor: Colors.black,
+    ),
+  );
+
+  static final ThemeData darkTheme = ThemeData(
+    useMaterial3: true,
+    colorScheme: ColorScheme.fromSeed(
+      seedColor: Colors.blue,
+      primary: Colors.blue,
+      brightness: Brightness.dark,
+    ),
+    textTheme: GoogleFonts.interTextTheme(ThemeData.dark().textTheme),
+    appBarTheme: const AppBarTheme(centerTitle: true, elevation: 0),
+  );
+}
